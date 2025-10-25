e5bfae9b68f58665dc2715a1b552d978157c2048
Oguzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2025-10-25 23:06:45 +0300

    styled components - intro

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,5 +1,18 @@
+import styled from 'styled-components';
+
+const StyledBtn = styled.button`
+  font-size: 1.5rem;
+  background: red;
+  color: white;
+`;
+
 const Landing = () => {
-  return <h1>Landing Page</h1>;
+  return (
+    <div>
+      <h1>Landing Page</h1>
+      <StyledBtn>Click Me</StyledBtn>
+    </div>
+  );
 };
 
 export default Landing;
