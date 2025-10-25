e129a797380cc20a36e441ba6da60e5cf87fe94f
Oguzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2025-10-26 02:10:54 +0300

    landing page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,23 +1,35 @@
-import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/LandingPage';
+import main from '../assets/images/main.svg';
+import logo from '../assets/images/logo.png';
 
 const Landing = () => {
   return (
     <Wrapper>
-      <h1>Landing Page</h1>
-      <div className="content">Some content</div>
+      <nav>
+        <img src={logo} alt="joobiq" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Welcome to Joobiq, your ultimate job tracking app designed to help
+            you manage and streamline your job search process with ease and
+            efficiency.
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
     </Wrapper>
   );
 };
-
-const Wrapper = styled.div`
-  background: red;
-  h1 {
-    color: white;
-  }
-  .content {
-    background: blue;
-    color: yellow;
-  }
-`;
 
 export default Landing;
