e47e18c5eeffe40e31a7c4e36638349b780f92c0
Oguzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2025-10-26 02:47:29 +0300

    landing page - css (optional)

--- client/src/assets/wrappers/LandingPage.js
+++ client/src/assets/wrappers/LandingPage.js
@@ -9,39 +9,48 @@ const Wrapper = styled.section`
     display: flex;
     align-items: center;
   }
+
   .page {
     min-height: calc(100vh - var(--nav-height));
     display: grid;
     align-items: center;
     margin-top: -3rem;
   }
+
   h1 {
     font-weight: 700;
+    margin-bottom: 1.5rem;
+
     span {
       color: var(--primary-500);
     }
-    margin-bottom: 1.5rem;
   }
+
   p {
     line-height: 2;
     color: var(--text-secondary-color);
     margin-bottom: 1.5rem;
     max-width: 35em;
   }
+
   .register-link {
     margin-right: 1rem;
   }
+
   .main-img {
     display: none;
   }
+
   .btn {
     padding: 0.75rem 1rem;
   }
+
   @media (min-width: 992px) {
     .page {
-      grid-template-columns: 1fr 400px;
+      grid-template-columns: 1fr 1fr;
       column-gap: 3rem;
     }
+
     .main-img {
       display: block;
     }
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -15,9 +15,10 @@ const Landing = () => {
             job <span>tracking</span> app
           </h1>
           <p>
-            Welcome to Joobiq, your ultimate job tracking app designed to help
-            you manage and streamline your job search process with ease and
-            efficiency.
+            Welcome to Joobiq — the intelligent job tracking platform designed
+            to streamline your job search, organize applications, and empower
+            you to achieve career success with clarity, confidence, and
+            efficiency every step.
           </p>
           <Link to="/register" className="btn register-link">
             Register
