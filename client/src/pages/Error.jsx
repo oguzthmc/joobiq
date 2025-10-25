186921a7c3d83170091090e4bbc41507ecd4c971
Oguzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2025-10-25 18:13:45 +0300

    error page

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,5 +1,15 @@
+import { Link, useRouteError } from 'react-router-dom';
+
 const Error = () => {
-  return <h1>Error Page</h1>;
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div>
+      <h1>Error Page</h1>
+      <Link to="/">Go to Home Page</Link>
+    </div>
+  );
 };
 
 export default Error;
