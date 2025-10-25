41c4da8584c096ab6a82ee7c5c4d7f3bc5b38c38
Oguzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2025-10-25 12:13:39 +0300

    react router - initial setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <div>
+        <h1>Home</h1>
+        <a href="about">Go to About page</a>
+      </div>
+    ),
+  },
+  {
+    path: '/about',
+    element: (
+      <div>
+        <h1>About page</h1>
+        <a href="/">Back to Home</a>
+      </div>
+    ),
+  },
+]);
 
 const App = () => {
-  return <div>Joobiq App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
