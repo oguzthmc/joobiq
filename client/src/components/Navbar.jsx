d1f9a674a0cba089eaf1f4583505e1182f852987
Oguzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2025-10-26 18:43:58 +0300

    react icons

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import Wrapper from '../assets/wrappers/Navbar';
+import { FaHome } from 'react-icons/fa';
 
 const Navbar = () => {
-  return <Wrapper>Navbar</Wrapper>;
+  return (
+    <Wrapper>
+      <h2>Navbar</h2>
+      <FaHome />
+    </Wrapper>
+  );
 };
 
 export default Navbar;
