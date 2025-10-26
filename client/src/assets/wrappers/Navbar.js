d16f2c8612282a6de79f819b58b49061b67d4b79
Oguzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2025-10-26 22:19:32 +0300

    navbar css (optional)

--- client/src/assets/wrappers/Navbar.js
+++ client/src/assets/wrappers/Navbar.js
@@ -7,12 +7,14 @@ const Wrapper = styled.nav`
   justify-content: center;
   box-shadow: 0 1px 0 0 rgba(0, 0, 0, 0.1);
   background: var(--background-secondary-color);
+
   .nav-center {
     display: flex;
     width: 90vw;
     align-items: center;
     justify-content: space-between;
   }
+
   .toggle-btn {
     background: transparent;
     border-color: transparent;
@@ -22,27 +24,34 @@ const Wrapper = styled.nav`
     display: flex;
     align-items: center;
   }
+
   .logo-text {
     display: none;
   }
+
   .logo {
     display: flex;
     align-items: center;
     width: 100px;
   }
+
   .btn-container {
     display: flex;
     align-items: center;
   }
+
   @media (min-width: 992px) {
     position: sticky;
     top: 0;
+
     .nav-center {
       width: 90%;
     }
+
     .logo {
       display: none;
     }
+
     .logo-text {
       display: block;
     }
