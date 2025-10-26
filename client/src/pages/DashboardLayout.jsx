84d1542cb5be725cae2f536d3f493590016c54ff
Oguzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2025-10-26 17:29:05 +0300

    dashboard context

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,23 +1,61 @@
 import { Outlet } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/Dashboard';
 import { BigSidebar, Navbar, SmallSidebar } from '../components';
+import React, { createContext, useContext, useState } from 'react';
+
+const DashboardContext = createContext();
 
 const DashboardLayout = () => {
+  //temp
+  const user = { name: 'John' };
+
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleDarkTheme = () => {
+    // setIsDarkTheme(() => !isDarkTheme);
+    console.log('toggle dark theme');
+  };
+
+  const toggleSidebar = () => {
+    setShowSidebar(() => !showSidebar);
+  };
+
+  const logoutUser = async () => {
+    // Implement logout functionality here
+    console.log('User logged out');
+  };
+
   return (
-    <Wrapper>
-      <main className="dashboard">
-        <SmallSidebar />
-        <BigSidebar />
-        <div>
-          <Navbar />
-          <div className="dashboard-page">
-            {/* Nested routes will render here */}
-            <Outlet />
+    <DashboardContext.Provider
+      value={{
+        user,
+        logoutUser,
+        showSidebar,
+        toggleSidebar,
+        isDarkTheme,
+        toggleDarkTheme,
+      }}
+    >
+      <Wrapper>
+        <main className="dashboard">
+          <SmallSidebar />
+          <BigSidebar />
+          <div>
+            <Navbar />
+            <div className="dashboard-page">
+              {/* Nested routes will render here */}
+              <Outlet />
+            </div>
           </div>
-        </div>
-      </main>
-    </Wrapper>
+        </main>
+      </Wrapper>
+    </DashboardContext.Provider>
   );
+};
+
+export const useDashboardContext = () => {
+  return useContext(DashboardContext);
 };
 
 export default DashboardLayout;
