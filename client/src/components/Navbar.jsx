7bb1e6a2076e443801f027a0456e066b6899ebba
Oguzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2025-10-28 01:16:52 +0300

    logout container

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import Wrapper from '../assets/wrappers/Navbar';
 import { FaAlignLeft } from 'react-icons/fa';
 import Logo from './Logo';
 import { useDashboardContext } from '../pages/DashboardLayout';
+import LogoutContainer from './LogoutContainer';
 
 const Navbar = () => {
   const { toggleSidebar } = useDashboardContext();
@@ -16,7 +17,9 @@ const Navbar = () => {
           <Logo />
           <h4 className="logo-text">Dashboard</h4>
         </div>
-        <div className="btn-container">Toogle/Logout</div>
+        <div className="btn-container">
+          <LogoutContainer />
+        </div>
       </div>
     </Wrapper>
   );
