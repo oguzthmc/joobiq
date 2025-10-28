7f961ed66ff874b7b11af4dbac0de0a00ae5517c
Oguzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2025-10-28 13:29:51 +0300

    logout container css (optional)

--- client/src/assets/wrappers/LogoutContainer.js
+++ client/src/assets/wrappers/LogoutContainer.js
@@ -2,17 +2,20 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
   position: relative;
+
   .logout-btn {
     display: flex;
     align-items: center;
     justify-content: center;
     gap: 0 0.5rem;
   }
+
   .img {
     width: 25px;
     height: 25px;
     border-radius: 50%;
   }
+
   .dropdown {
     position: absolute;
     top: 45px;
@@ -24,9 +27,11 @@ const Wrapper = styled.div`
     border-radius: var(--border-radius);
     background: var(--primary-500);
   }
+
   .show-dropdown {
     visibility: visible;
   }
+
   .dropdown-btn {
     border-radius: var(--border-radius);
     padding: 0.5rem;
