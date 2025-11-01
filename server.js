39056edb1cfe02f39c4827eca01f983a0711a637
Oguzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2025-11-01 17:29:08 +0300

    morgan & dotenv

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
+import * as dotenv from 'dotenv';
 import express from 'express';
+import morgan from 'morgan';
+
+dotenv.config();
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
 
@@ -12,6 +20,8 @@ app.post('/', (req, res) => {
   res.json({ message: 'data received', data: req.body });
 });
 
-app.listen(5100, () => {
-  console.log('server running...');
+const port = process.env.PORT || 5100;
+
+app.listen(port, () => {
+  console.log(`server running on PORT ${port}...`);
 });
