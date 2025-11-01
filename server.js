793d888b6dc355c1e95fe7efff7a26c80021026f
Oguzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2025-11-01 15:45:23 +0300

    json middleware

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 import express from 'express';
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+app.post('/', (req, res) => {
+  console.log(req);
+  res.json({ message: 'data received', data: req.body });
 });
 
 app.listen(5100, () => {
