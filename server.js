68ebbd996dbc5557a00427de05f8ad27524894ee
Oguzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2025-11-03 17:13:28 +0300

    edit job

--- server.js
+++ server.js
@@ -54,6 +54,25 @@ app.get('/api/v1/jobs/:id', (req, res) => {
   res.status(200).json({ job });
 });
 
+// EDIT JOB
+app.patch('/api/v1/jobs/:id', (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    res.status(400).json({ msg: 'please provide company and position' });
+    return;
+  }
+
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+
+  job.company = company;
+  job.position = position;
+  res.status(200).json({ msg: 'job modified', job });
+});
+
 const port = process.env.PORT || 5100;
 
 app.listen(port, () => {
