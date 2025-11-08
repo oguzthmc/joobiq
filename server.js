28b7f29f69d1edd4ca283a86725a70566f45e68c
Oguzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2025-11-08 13:53:48 +0300

    authenticate user setup

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ import authRouter from './routes/authRouter.js';
 
 // middleware
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
+import { authenticateUser } from './middleware/authMiddleware.js';
 
 dotenv.config();
 const app = express();
@@ -24,7 +25,7 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.use('/api/v1/jobs', jobRouter);
+app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/auth', authRouter);
 
 // Not Found Middleware
