0cdd78acb029cf78e80ae1f5873ff1eac897a113
Oguzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2025-11-14 02:13:55 +0300

    get current user

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,19 +1,22 @@
-import { Outlet, useLoaderData } from 'react-router-dom';
+import { Outlet, redirect, useLoaderData } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/Dashboard';
 import { BigSidebar, Navbar, SmallSidebar } from '../components';
 import React, { createContext, useContext, useState } from 'react';
+import customFetch from '../utils/customFetch';
 
-export const loader = () => {
-  return 'hello world';
+export const loader = async () => {
+  try {
+    const { data } = await customFetch.get('/users/current-user');
+    return data;
+  } catch (error) {
+    return redirect('/');
+  }
 };
 
 const DashboardContext = createContext();
 
 const DashboardLayout = ({ isDarkThemeEnabled }) => {
-  const data = useLoaderData();
-  console.log(data);
-  //temp
-  const user = { name: 'John' };
+  const { user } = useLoaderData();
 
   const [showSidebar, setShowSidebar] = useState(false);
   const [isDarkTheme, setIsDarkTheme] = useState(isDarkThemeEnabled);
@@ -54,7 +57,7 @@ const DashboardLayout = ({ isDarkThemeEnabled }) => {
             <Navbar />
             <div className="dashboard-page">
               {/* Nested routes will render here */}
-              <Outlet />
+              <Outlet context={{ user }} />
             </div>
           </div>
         </main>
