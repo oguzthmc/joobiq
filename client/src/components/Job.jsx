dd46fc082a385738f6021d2cafd4f838869813b6
Oguzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2025-11-16 13:26:52 +0300

    jobs container jsx

---
+++ client/src/components/Job.jsx
@@ -0,0 +1,5 @@
+const Job = () => {
+  return <h3>Job</h3>;
+};
+
+export default Job;
--- client/src/components/JobsContainer.jsx
+++ client/src/components/JobsContainer.jsx
@@ -1,5 +1,28 @@
+import Wrapper from '../assets/wrappers/JobsContainer';
+import { useAllJobsContext } from '../pages/AllJobs';
+import Job from './Job';
+
 const JobsContainer = () => {
-  return <h2>JobsContainer</h2>;
+  const { data } = useAllJobsContext();
+  const { jobs } = data;
+
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display...</h2>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper>
+      <div className="jobs">
+        {jobs.map((job) => {
+          return <Job key={job._id} {...job} />;
+        })}
+      </div>
+    </Wrapper>
+  );
 };
 
 export default JobsContainer;
