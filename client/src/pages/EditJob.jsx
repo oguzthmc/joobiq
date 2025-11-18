2ae954d4e0781013339466a737363d2f8e5be704
Oguzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2025-11-18 13:22:32 +0300

    route params

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,4 +1,18 @@
+import { useParams } from 'react-router-dom';
+
+export const loader = async ({ params }) => {
+  console.log(params);
+  return null;
+};
+
+export const action = async () => {
+  return null;
+};
+
 const EditJob = () => {
+  const params = useParams();
+  console.log('Editing job with ID:', params);
+
   return <h1>EditJob Page</h1>;
 };
 
