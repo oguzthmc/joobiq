3a5e4fbd293f211e2977720ea7713fac7e331c05
Oguzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2025-11-22 15:51:49 +0300

    profile action

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,26 @@
 import { Form, useNavigation, useOutletContext } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/DashboardFormPage';
 import { FormRow } from '../components';
+import { toast } from 'react-toastify';
+import customFetch from '../utils/customFetch';
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const file = formData.get('avatar');
+
+  if (file && file.size > 500000) {
+    toast.error('Image size too large');
+    return null;
+  }
+
+  try {
+    await customFetch.patch('/users/update-user', formData);
+    toast.success('Profile updated successfully');
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+  }
+  return null;
+};
 
 const Profile = () => {
   const { user } = useOutletContext();
