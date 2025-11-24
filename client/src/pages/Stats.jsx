351518818c91c81588dfefe618457ef26fbd9997
Oguzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2025-11-24 13:24:49 +0300

    stats page setup

--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -1,5 +1,27 @@
+import { useLoaderData } from 'react-router-dom';
+import customFetch from '../utils/customFetch';
+import { ChartsContainer, StatsContainer } from '../components';
+
+export const loader = async () => {
+  try {
+    const response = await customFetch.get('/jobs/stats');
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
+
 const Stats = () => {
-  return <h1>Stats Page</h1>;
+  const { defaultStats, monthlyApplications } = useLoaderData();
+
+  return (
+    <>
+      <StatsContainer defaultStats={defaultStats} />
+      {monthlyApplications?.length > 1 && (
+        <ChartsContainer data={monthlyApplications} />
+      )}
+    </>
+  );
 };
 
 export default Stats;
