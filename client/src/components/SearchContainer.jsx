7b84818f5d7874a8e580130f57ca68e0c945104a
Oguzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2025-11-26 14:24:24 +0300

    search form setup

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -1,5 +1,42 @@
+import { FormRow, FormRowSelect, SubmitBtn } from '.';
+import Wrapper from '../assets/wrappers/DashboardFormPage';
+import { Form, Link } from 'react-router-dom';
+import { JOB_TYPE, JOB_STATUS, JOB_SORT_BY } from '../../../utils/constants';
+
 const SearchContainer = () => {
-  return <h2>SearchContainer</h2>;
+  return (
+    <Wrapper>
+      <Form className="form">
+        <h5 className="form-title">search form</h5>
+        <div className="form-center">
+          <FormRow type="search" name="search" defaultValue="a" />
+
+          <FormRowSelect
+            labelText="job status"
+            name="jobStatus"
+            list={['all', ...Object.values(JOB_STATUS)]}
+            defaultValue="all"
+          />
+          <FormRowSelect
+            labelText="job type"
+            name="jobType"
+            list={['all', ...Object.values(JOB_TYPE)]}
+            defaultValue="all"
+          />
+          <FormRowSelect
+            name="sort"
+            list={[...Object.values(JOB_SORT_BY)]}
+            defaultValue="newest"
+          />
+          <Link to="/dashboard/all-jobs" className="btn form-btn delete-btn">
+            Reset Search Values
+          </Link>
+          {/* TEMP !!!! */}
+          <SubmitBtn formBtn />
+        </div>
+      </Form>
+    </Wrapper>
+  );
 };
 
 export default SearchContainer;
