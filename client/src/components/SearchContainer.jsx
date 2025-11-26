1918385ce0d411194a5804c4a66629ca635cda06
Oguzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2025-11-26 23:04:42 +0300

    controlled inputs

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -1,32 +1,53 @@
 import { FormRow, FormRowSelect, SubmitBtn } from '.';
 import Wrapper from '../assets/wrappers/DashboardFormPage';
-import { Form, Link } from 'react-router-dom';
+import { Form, Link, useSubmit } from 'react-router-dom';
 import { JOB_TYPE, JOB_STATUS, JOB_SORT_BY } from '../../../utils/constants';
+import { useAllJobsContext } from '../pages/AllJobs';
 
 const SearchContainer = () => {
+  const { searchValues } = useAllJobsContext();
+  const { search, jobStatus, jobType, sort } = searchValues;
+  const submit = useSubmit();
+
   return (
     <Wrapper>
       <Form className="form">
         <h5 className="form-title">search form</h5>
         <div className="form-center">
-          <FormRow type="search" name="search" defaultValue="a" />
+          <FormRow
+            type="search"
+            name="search"
+            defaultValue={search}
+            onChange={(e) => {
+              submit(e.currentTarget.form);
+            }}
+          />
 
           <FormRowSelect
             labelText="job status"
             name="jobStatus"
             list={['all', ...Object.values(JOB_STATUS)]}
-            defaultValue="all"
+            defaultValue={jobStatus}
+            onChange={(e) => {
+              submit(e.currentTarget.form);
+            }}
           />
           <FormRowSelect
             labelText="job type"
             name="jobType"
             list={['all', ...Object.values(JOB_TYPE)]}
-            defaultValue="all"
+            defaultValue={jobType}
+            onChange={(e) => {
+              submit(e.currentTarget.form);
+            }}
           />
           <FormRowSelect
             name="sort"
             list={[...Object.values(JOB_SORT_BY)]}
-            defaultValue="newest"
+            defaultValue={sort}
+            onChange={(e) => {
+              submit(e.currentTarget.form);
+            }}
           />
           <Link to="/dashboard/all-jobs" className="btn form-btn delete-btn">
             Reset Search Values
--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -5,15 +5,13 @@ import { JobsContainer, SearchContainer } from '../components';
 import { createContext, useContext } from 'react';
 
 export const loader = async ({ request }) => {
-  console.log(request.url);
   const params = Object.fromEntries([
     ...new URL(request.url).searchParams.entries(),
   ]);
-  console.log(params);
 
   try {
     const { data } = await customFetch.get('/jobs', { params });
-    return { data };
+    return { data, searchValues: { ...params } };
   } catch (error) {
     toast.error(error?.response?.data?.msg);
     return error;
@@ -23,9 +21,10 @@ export const loader = async ({ request }) => {
 const AllJobsContext = createContext();
 
 const AllJobs = () => {
-  const { data } = useLoaderData();
+  const { data, searchValues } = useLoaderData();
+
   return (
-    <AllJobsContext.Provider value={{ data }}>
+    <AllJobsContext.Provider value={{ data, searchValues }}>
       <SearchContainer />
       <JobsContainer />
     </AllJobsContext.Provider>
