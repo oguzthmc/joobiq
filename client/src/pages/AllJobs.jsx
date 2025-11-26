3fef21656c4b5d23193fd4c760027af5bb2f9481
Oguzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2025-11-26 15:28:25 +0300

    loader & query params

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -4,9 +4,15 @@ import { useLoaderData } from 'react-router-dom';
 import { JobsContainer, SearchContainer } from '../components';
 import { createContext, useContext } from 'react';
 
-export const loader = async () => {
+export const loader = async ({ request }) => {
+  console.log(request.url);
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ]);
+  console.log(params);
+
   try {
-    const { data } = await customFetch('/jobs');
+    const { data } = await customFetch.get('/jobs', { params });
     return { data };
   } catch (error) {
     toast.error(error?.response?.data?.msg);
