d68417ca647b3169f56a6bbf5187e0ea01e031df
Oguzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2025-11-29 13:28:39 +0300

    pagination logic - complex approach

--- client/src/components/PageBtnContainer.jsx
+++ client/src/components/PageBtnContainer.jsx
@@ -20,6 +20,72 @@ const PageBtnContainer = () => {
     navigate(`${pathname}?${searchParams.toString()}`);
   };
 
+  const addPageButton = ({ pageNumber, activeClass }) => {
+    return (
+      <button
+        className={`btn page-btn ${activeClass && 'active'}`}
+        key={pageNumber}
+        onClick={() => handlePageChange(pageNumber)}
+      >
+        {pageNumber}
+      </button>
+    );
+  };
+
+  const renderPageButtons = () => {
+    const pageButtons = [];
+    // first page
+    pageButtons.push(
+      addPageButton({ pageNumber: 1, activeClass: currentPage === 1 })
+    );
+    //dots after first page
+    if (currentPage > 3) {
+      pageButtons.push(
+        <span key="dots-1" className="page-btn dots">
+          ...
+        </span>
+      );
+    }
+    // one before current page
+    if (currentPage !== 1 && currentPage !== 2) {
+      pageButtons.push(
+        addPageButton({ pageNumber: currentPage - 1, activeClass: false })
+      );
+    }
+    // current page
+    if (currentPage != 1 && currentPage != numOfPages) {
+      pageButtons.push(
+        addPageButton({
+          pageNumber: currentPage,
+          activeClass: true,
+        })
+      );
+    }
+    // one after current page
+    if (currentPage !== numOfPages && currentPage !== numOfPages - 1) {
+      pageButtons.push(
+        addPageButton({ pageNumber: currentPage + 1, activeClass: false })
+      );
+    }
+    // dots before last page
+    if (currentPage < numOfPages - 2) {
+      pageButtons.push(
+        <span key="dots+1" className="page-btn dots">
+          ...
+        </span>
+      );
+    }
+    // last page
+    pageButtons.push(
+      addPageButton({
+        pageNumber: numOfPages,
+        activeClass: currentPage === numOfPages,
+      })
+    );
+
+    return pageButtons;
+  };
+
   return (
     <Wrapper>
       <button
@@ -33,21 +99,7 @@ const PageBtnContainer = () => {
         <HiChevronDoubleLeft />
         prev
       </button>
-      <div className="btn-container">
-        {pages.map((pageNumber) => {
-          return (
-            <button
-              className={`btn page-btn ${
-                pageNumber === currentPage && 'active'
-              }`}
-              key={pageNumber}
-              onClick={() => handlePageChange(pageNumber)}
-            >
-              {pageNumber}
-            </button>
-          );
-        })}
-      </div>
+      <div className="btn-container">{renderPageButtons()}</div>
       <button
         className="btn next-btn"
         onClick={() => {
